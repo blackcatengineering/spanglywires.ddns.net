45f9a5ccb299d5c55d9567c12d1557307b9689f9
Terraform User <builder-admin@spanglywires.ddns.net>
2026-02-03 11:11:21 +0000

    Managed by Terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 module "builder-infra" {
   source                 = "git@github.com:blackcatengineering/tf-mod-builder_infra"
   domain                 = "${var.domain}"
-  enable_istio           = false
+  enable_istio           = true
   enable_jenkins         = true
   enable_ip_masq         = true
   enable_harness         = true
   enable_cert_manager    = true
-  enable_trust_manager   = true
   enable_jaeger          = true
   harness_account_id     = var.harness_account_id
   harness_delegate_token = var.harness_delegate_token
